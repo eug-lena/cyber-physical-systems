5fbaaf6a4e144b44c76253b56878c276866eaf93
Adrian Hassa <adrianhassa@gmail.com>
2024-03-27 19:53:51 +0100

    #3 Add comments to the include directives

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include "PrimeChecker.hpp"
+#include <iostream>			// Include a input/output library
+#include "PrimeChecker.hpp"	// Include a header file for the program checking if a number is prime
 
 int main(int argc, char** argv) {
 	if (argc == 2) {
