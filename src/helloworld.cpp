ae74c598459b9d585cb36ecd8f6057d9f6e919d6
Teo Portase <teoportase@gmail.com>
2024-03-27 20:01:10 +0100

    #4 Add comments to helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -2,10 +2,15 @@
 #include "PrimeChecker.hpp"
 
 int main(int argc, char** argv) {
+	// Check if the number of arguments is correct
 	if (argc == 2) {
+		// Convert the string received into an integer value
 		int number = std::stoi(argv[1]);
+		// Declare a primechecker variable
 		PrimeChecker pc;
+		// Print out a message to the console
 		std::cout << "Group, 18; " << number << " is a prime number? " << pc.isPrime(number) << std::endl;
 	}
+	// Exit the program
 	return 0;
 }
