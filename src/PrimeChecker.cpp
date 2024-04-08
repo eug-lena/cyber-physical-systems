7e9c13c1d56ac4d3d982cf41cf406b1ef7a11848
Teo Portase <teoportase@gmail.com>
2024-04-08 16:17:00 +0200

    Fail the PrimeChecker tests
    Changed the correct line of code this time, so that retVal is never true.

--- src/PrimeChecker.cpp
+++ src/PrimeChecker.cpp
@@ -2,14 +2,14 @@
 
 // This method checks if the given number is a prime number or not
 bool PrimeChecker::isPrime(uint16_t n) {
-    bool retVal{true};
+    bool retVal{false};
     if (n<2 || 0 == n%2) {
-        retVal = true;
+        retVal = false;
     }
     else {
         for(uint16_t i{3}; (i*i) <= n; i += 2) {
             if (0 == n%i) {
-                return true;
+                return false;
                 break;
             }
         }
