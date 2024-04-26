97e66e0853ed685470c5e616bef5fc186f0e6596
Adrian Hassa <adrianhassa@gmail.com>
2024-04-26 16:37:54 +0200

    #3 Remove build files

--- src/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adrian/Desktop/2024-group-18/src
-# Build directory: /home/adrian/Desktop/2024-group-18/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(helloworld-Runner "/home/adrian/Desktop/2024-group-18/src/build/helloworld-Runner")
-set_tests_properties(helloworld-Runner PROPERTIES  _BACKTRACE_TRIPLES "/home/adrian/Desktop/2024-group-18/src/CMakeLists.txt;8;add_test;/home/adrian/Desktop/2024-group-18/src/CMakeLists.txt;0;")
