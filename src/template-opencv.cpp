c6c171350890519122bf9f9ee5f50497a93f445d
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-05-10 12:58:30 +0200

    #18 Refactor image processing

--- src/template-opencv.cpp
+++ src/template-opencv.cpp
@@ -24,6 +24,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include "ImageDenoiser.hpp"
+
 // Lower threshold for detecting blue cones
 cv::Scalar blueLow = cv::Scalar(107, 111, 45);
 // Higher threshold for detecting blue cones
@@ -128,37 +130,6 @@ static void onYellowTrackbar(int value, void *userdata)
     default:
         break;
     }
-}
-
-void denoiseImage(cv::Mat &originalImage, cv::Mat &colorMask, cv::Mat &processedImage, int thresholdValue, int maxValue)
-{
-    // Create a copy of the colorMask to preserve the original data
-    cv::Mat colorMaskCopy;
-    colorMask.copyTo(colorMaskCopy);
-
-    // Apply Gaussian Blur to the color mask to reduce noise
-    cv::GaussianBlur(colorMaskCopy, colorMaskCopy, cv::Size(5, 5), 0);
-
-    // Apply Closing operation to the color mask to improve quality
-    cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-    cv::morphologyEx(colorMaskCopy, colorMaskCopy, cv::MORPH_CLOSE, element);
-
-    // Create a mask to ignore the car at the bottom-center of the image
-    cv::Mat mask = cv::Mat::ones(originalImage.size(), CV_8U);
-    cv::Rect ignoreRegion(mask.cols / 4, 3 * mask.rows / 4, mask.cols / 2, mask.rows / 4);
-    mask(ignoreRegion) = 0;
-
-    // Apply the mask to the colorMaskCopy
-    colorMaskCopy = colorMaskCopy & mask;
-
-    // Perform a Bitwise And operation to extract the color information from the original image
-    cv::bitwise_and(originalImage, originalImage, processedImage, colorMaskCopy);
-
-    // Convert the processed image to Grayscale
-    cv::cvtColor(processedImage, processedImage, cv::COLOR_BGR2GRAY);
-
-    // Apply a Threshold to the processed image
-    cv::threshold(processedImage, processedImage, thresholdValue, maxValue, cv::THRESH_BINARY);
 }
 
 int32_t main(int32_t argc, char **argv)
@@ -335,8 +306,8 @@ int32_t main(int32_t argc, char **argv)
                 cv::Mat processedYellow;
 
                 // Denoise processed images
-                denoiseImage(outputImage, maskBlue, processedBlue, blueThreshold, blueMaxValue);
-                denoiseImage(outputImage, maskYellow, processedYellow, yellowThreshold, yellowMaxValue);
+                ImageDenoiser::denoiseImage(outputImage, maskBlue, processedBlue, blueThreshold, blueMaxValue);
+                ImageDenoiser::denoiseImage(outputImage, maskYellow, processedYellow, yellowThreshold, yellowMaxValue);
 
                 // Initialize an array of contours
                 std::vector<std::vector<cv::Point>> contoursBlue;
